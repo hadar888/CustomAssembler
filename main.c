d7fcca910faa124c734483bb0c379011c0b48a9e
mllm <119107076+mlsopenu@users.noreply.github.com>
2024-03-24 20:05:21 +0200

    comments in main for future code

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "constants.h"
 
 int main(void) {
-    printf("Hello, World!\n");
+    /*here will be a macro command that get the file and returns a new file with macro spread out*/
+    /*here we go through the file to check for syntax errors*/
     return 0;
 }
